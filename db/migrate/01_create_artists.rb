3002959ee2547d4209958e3a7359a0dd81f23aa8
Dennis Chung <chungdennis6@gmail.com>
2019-06-28 13:43:00 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,5 +3,13 @@ class CreateArtists < ActiveRecord::Migration
   end
  
   def down
+ def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
+
 end
